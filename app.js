4844c31bdc4105fc80b7301a1155ab0f2a09f8ae
Dirk Verest <d.verest@gmail.com>
2020-01-04 20:57:01 -0500

    Heroku Setup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mainRoutes = require('./routes'); // Setup routes (gets index.js as a default)
 const errorHandler = require('./errorHandlers.js');
 // Express Settings
